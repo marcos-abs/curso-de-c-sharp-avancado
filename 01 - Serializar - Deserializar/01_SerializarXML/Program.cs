9e7d5c4903fd063fa6f79493afdf45038c31c805
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-07 11:37:02 -0300

    primeira aula (pausada)

---
+++ 01 - Serializar - Deserializar/01_SerializarXML/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // Serialização para arquivos
+using _00_Biblioteca;
+using System.Xml.Serialization; // Serialização em XML
+
+namespace _01_SerializarXML {
+    class Program {
+        static void Main(string[] args) {
+            Usuario usuario = new Usuario() { Nome = "José Costa Silva", CPF = "222.222.222-22", Email = "jose@gmail.com" };
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+
+        }
+    }
+}
