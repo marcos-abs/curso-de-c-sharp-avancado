7e7ea815102513e9fad6563e11adb0ddab5ddf27
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-07 16:34:13 -0300

    4. Deserializar no formato XML.

--- 01 - Serializar - Deserializar/01_SerializarXML/Program.cs
+++ 01 - Serializar - Deserializar/01_SerializarXML/Program.cs
@@ -11,8 +11,12 @@ namespace _01_SerializarXML {
     class Program {
         static void Main(string[] args) {
             Usuario usuario = new Usuario() { Nome = "José Costa Silva", CPF = "222.222.222-22", Email = "jose@gmail.com" };
-            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario)); // uma das formas possíveis
+            // XmlSerializer serializador = new XmlSerializer(usuario.GetType()); // outra forma possível
 
+            StreamWriter stream = new StreamWriter(@"C:\Users\Marcos Antônio\Documents\Curso de C# Avançado\01 - Serializar - Deserializar\01_SerializarXML.xml");
+
+            serializador.Serialize(stream, usuario);
         }
     }
 }
---
+++ 01 - Serializar - Deserializar/02_DeserializarXML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _00_Biblioteca;
+using System.IO; // Serialização para arquivos
+using System.Xml.Serialization; // Serialização em XML
+
+
+namespace _02_DeserializarXML {
+    class Program {
+        static void Main(string[] args) {
+            StreamReader stream = new StreamReader(@"C:\Users\Marcos Antônio\Documents\Curso de C# Avançado\01 - Serializar - Deserializar\01_SerializarXML.xml");
+
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+
+            Usuario usuario = (Usuario) serializador.Deserialize(stream);
+
+            Console.WriteLine("\nUsuario (nome): {0}, CPF: {1}, e Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            Console.ReadKey();
+        }
+    }
+}
