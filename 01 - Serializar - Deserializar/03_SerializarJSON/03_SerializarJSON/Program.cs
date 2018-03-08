55fc43a1a4cfba2d04c96eaeceefc1e03d191232
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-07 23:11:42 -0300

    6. Deserializar no formato JSON.

---
+++ 01 - Serializar - Deserializar/03_SerializarJSON/03_SerializarJSON/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using _00_Biblioteca;
+using System.IO;
+
+namespace _03_SerializarJSON {
+    class Program {
+        static void Main(string[] args) {
+            Usuario usuario = new Usuario() { Nome = "Maria Costa Silva", CPF = "333.333.333-00", Email = "maria@gmail.com" };
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            string stringObjSerializado = serializador.Serialize(usuario);
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\Marcos Antônio\Documents\Curso de C# Avançado\01 - Serializar - Deserializar\02_SerializarJSON.json");
+            sw.WriteLine(stringObjSerializado);
+            sw.Close();
+        }
+    }
+}
---
+++ 01 - Serializar - Deserializar/04_DeserializarJSON/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _00_Biblioteca;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace _04_DeserializarJSON {
+    class Program {
+        static void Main(string[] args) {
+            StreamReader stream = new StreamReader(@"C:\Users\Marcos Antônio\Documents\Curso de C# Avançado\01 - Serializar - Deserializar\02_SerializarJSON.json");
+            string linhasDoArquivo = stream.ReadToEnd();
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            Usuario usuario = (Usuario) serializador.Deserialize(linhasDoArquivo, typeof(Usuario));
+            Console.WriteLine("\nUsuario (nome): {0}, CPF: {1}, e Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            Console.ReadKey();
+        }
+    }
+}
