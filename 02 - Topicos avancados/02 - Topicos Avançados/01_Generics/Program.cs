91f146b003d018064ff12b762c01bbcfd907e020
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-12 12:32:30 -0300

    10. Generics - Parte 3.

---
+++ 02 - Topicos avancados/02 - Topicos Avançados/01_Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Generics.Modelo;
+
+namespace _01_Generics {
+    class Program {
+        static void Main(string[] args) {
+            Carro carro = new Carro() { Marca = "Fiat", Modelo = "Uno" };
+            Casa casa = new Casa() { Cidade = "Brasilia", Endereco = "QSQ 400" };
+            Usuario usuario = new Usuario() { Nome = "Maria", Email = "maria@gmail.com", Senha = "123456" };
+
+            Serializador.Serializar(carro);
+            Serializador.Serializar(casa);
+            Serializador.Serializar(usuario);
+
+            Carro carro2 = Serializador.Deserializar<Carro>();
+            Casa casa2 = Serializador.Deserializar<Casa>();
+            Usuario usuario2 = Serializador.Deserializar<Usuario>();
+
+            Console.WriteLine("\nCarro2: " + carro2.Marca + " - " + carro2.Modelo);
+            Console.WriteLine("\nCasa2: " + casa2.Endereco + " - " + casa2.Cidade);
+            Console.WriteLine("\nUsuario2: " + usuario2.Nome + " - " + usuario2.Email);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02 - Topicos avancados/02 - Topicos Avançados/01_Generics/Serializador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace _01_Generics {
+    public class Serializador {
+        /* Serializador sem Generics */
+        public static void Serializar(Object obj) {
+            StreamWriter sw = new StreamWriter(@"C:\Users\Marcos Antônio\Documents\Curso de C# Avançado\02 - Topicos avancados\03_" + obj.GetType().Name + ".txt");
+
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            string objSerializado = serializador.Serialize(obj);
+
+            sw.Write(objSerializado);
+            sw.Close();
+        }
+
+        /* Deserializador com Generics */
+        public static T Deserializar <T>() {
+            StreamReader sr = new StreamReader(@"C:\Users\Marcos Antônio\Documents\Curso de C# Avançado\02 - Topicos avancados\03_" + typeof(T).Name + ".txt");
+            string conteudo = sr.ReadToEnd();
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            T obj = (T) serializador.Deserialize(conteudo, typeof(T));
+            return obj;
+        }
+    }
+}
