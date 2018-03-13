ef7a89874d301ca2b36d94fca833ed8ec4ec2e7f
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-13 20:58:26 -0300

    16. Sintaxe alternativa LINQ.

---
+++ 02 - Topicos avancados/Secao_04/01_Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Linq {
+    class Program {
+        static void Main(string[] args) {
+            // Linq e Lambda
+            // Lambda = (entrada) => (expressão)
+            int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 };
+            // var listaFiltrada = lista.Where(a => a > 10).Select(a => a); // ordem natural
+            //var listaFiltrada = lista.Where(a => a > 10).OrderBy(a=>a).Select(a => a); //ordem ascendente
+            var listaFiltrada = lista.Where(a => a > 10).OrderByDescending(a => a).Select(a => a); //ordem decrescente
+            foreach (var item in listaFiltrada) {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02 - Topicos avancados/Secao_04/02_Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Linq {
+    class Program {
+        static void Main(string[] args) {
+            // Linq e Lambda
+            // Lambda = (entrada) => (expressão)
+            int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 };
+            // var listaFiltrada = lista.Where(a => a > 10).Select(a => a); // ordem natural - exemplo com linq
+            var listaFiltrada = from a in lista where a > 10 orderby a select a; // exemplo de linq diferente
+            foreach (var item in listaFiltrada) {
+                Console.WriteLine("- - " + item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
