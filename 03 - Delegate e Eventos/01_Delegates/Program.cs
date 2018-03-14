a9fa45ed8dc7b60fff4098d65324780d3f51ff4e
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-14 16:58:54 -0300

    22. Delegate (codigo simples sem delegate).

---
+++ 03 - Delegate e Eventos/01_Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Delegates {
+    class Program {
+        static void Main(string[] args) {
+            var so = Soma(10, 20);
+            var su = Subtrai(20, 10);
+
+            Console.WriteLine("Soma: " + so);
+            Console.WriteLine("Sub: " + su);
+
+            Console.ReadKey();
+        }
+
+        static int Soma(int a, int b) {
+            return a + b;
+        }
+
+        static int Subtrai(int a, int b) {
+            return a - b;
+        }
+    }
+}
