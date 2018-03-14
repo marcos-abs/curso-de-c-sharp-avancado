eb7f0e0bba9305803a3d63d300c3fa9ad4b11060
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-13 22:11:16 -0300

    19. LINQ - Join - Parte 2.

---
+++ 02 - Topicos avancados/Secao_04/04_Linq_Join/Livro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Linq_Join {
+    class Livro {
+        public int Id { get; set; }
+        public int AutorId { get; set; }
+        public string Titulo { get; set; }
+        public string AnoPublicacao { get; set; }
+    }
+}
