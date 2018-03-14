4ce96a8c9727948ae7805bb4e30d1671b9acea7d
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-13 21:29:09 -0300

    17. LINQ com Objeto.

---
+++ 02 - Topicos avancados/Secao_04/03_Linq_Obj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Linq_Obj {
+    class Program {
+        static void Main(string[] args) {
+            List<Usuario> lista = new List<Usuario>();
+            lista.Add(new Usuario() { Nome = "José", Email = "jose@gmail.com" });
+            lista.Add(new Usuario() { Nome = "Maria", Email = "maria@hotmail.com" });
+            lista.Add(new Usuario() { Nome = "João", Email = "joao@ig.com" });
+            lista.Add(new Usuario() { Nome = "Felipe", Email = "felipe@gmail.com" });
+            lista.Add(new Usuario() { Nome = "Elias", Email = "elias@gmail.com" });
+
+            var ListaFiltrada = lista.Where(a => a.Email.Contains("@gmail.com")).OrderBy(a => a.Nome).Select(a => a); // ordenado por nome
+
+            foreach(var item in ListaFiltrada) {
+                Console.WriteLine(item.Nome + " - " + item.Email);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02 - Topicos avancados/Secao_04/03_Linq_Obj/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Linq_Obj {
+    class Usuario {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
