55de620d1aca5fb98943b3a3c7901d5619a4c0ee
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-30 20:57:02 -0300

    37. Thread - Recursos compartilhados - Thread Safe.

---
+++ 04 - Threads/04_ThreadSafe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _04_ThreadSafe {
+    class Program {
+
+        static int Rede = 0;
+        static object variavelDeControle = 0;
+
+        static void Main(string[] args) {
+            Console.WriteLine("Dataini: " + DateTime.Now);
+            for (int i = 0; i < 5; i++) {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.IsBackground = true;
+                t1.Start();
+            }
+            Console.WriteLine("\nPrograma terminado.");
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao() {
+            for (int i = 0; i < 1000; i++) {
+                lock (variavelDeControle) {
+                    Console.WriteLine("Numero: " + i);
+                }
+            }
+            Console.WriteLine("DataTime: " + DateTime.Now);
+        }
+    }
+}
