0b373c3a803b120f58a05aca6061149382592a43
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-30 20:29:33 -0300

    35. Thread - Multi Thread.

---
+++ 04 - Threads/02_MultiThread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _02_MultiThread {
+    class Program {
+        static void Main(string[] args) {
+            for(int i = 0; i < 5; i++) {
+                Thread t1 = new Thread(ThreadRepeticao);
+                t1.Start();
+            }
+            Console.WriteLine("\nPrograma terminado.");
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao() {
+            for (int i = 0; i < 1000; i++) {
+                Console.WriteLine("Numero: " + i);
+            }
+        }
+
+    }
+}
