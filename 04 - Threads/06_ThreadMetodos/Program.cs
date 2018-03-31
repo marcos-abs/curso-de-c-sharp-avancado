fbd00a36b6303c1d3d9a51dfcecca8c1e20b9e97
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-30 21:21:56 -0300

    39. Thread - Sleep e Join.

---
+++ 04 - Threads/06_ThreadMetodos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _06_ThreadMetodos {
+    class Program {
+        static void Main(string[] args) {
+            // thread sleep
+            Console.WriteLine("\nPrograma iniciado.");
+            Thread.Sleep(3000);
+            Console.WriteLine("\nRotina terminada.");
+            Thread t1 = new Thread(ThreadRepeticao);
+            Thread t2 = new Thread(ThreadRepeticao);
+            Thread t3 = new Thread(ThreadRepeticao);
+            Thread t4 = new Thread(ThreadRepeticao);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t1.Join();
+
+            Console.WriteLine("\nPrograma finalizado.");
+            Console.ReadKey();
+        }
+
+        static void ThreadRepeticao(object Id) {
+            for (int i = 0; i < 1000; i++) {
+                Console.WriteLine("Thread" + Id + " Numero: " + i + " Id:" + Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+    }
+}
