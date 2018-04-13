4ce777ac2e18080169f55a0275854b679e1e7a1c
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-04-13 13:41:21 -0300

    47. Validacao - Data Annotation - Parte 1.

---
+++ 06 - Atributos/01_Atributo/MeuAtributo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Atributo {
+    class MeuAtributo : Attribute {
+
+        [MeuAtributo("Atributo Classe", Descricao = "Descrição do Atributo")]
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public MeuAtributo(string nome) {
+            Nome = nome;
+        }
+    }
+}
---
+++ 06 - Atributos/02_Validacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace _02_Validacao {
+    class Program {
+        static void Main(string[] args) {
+            Usuario usuario = new Usuario() { Nome = "José", Email = "jose", Senha = "1234567" };
+
+            ValidationContext contexto = new ValidationContext(usuario);
+            List<ValidationResult> resultados = new List<ValidationResult>();
+
+            if(Validator.TryValidateObject(usuario, contexto, resultados, true) == false) { // valida todos requisitos
+                // if(Validator.TryValidateObject(usuario, contexto, resultados) == false) { // valida somente alguns requisitos
+
+                // mensagens
+                foreach (var erro in resultados) {
+                        Console.WriteLine(erro.ErrorMessage);
+                    }
+            }
+            Console.WriteLine("\nPrograma finalizado.");
+            Console.ReadKey();
+        }
+    }
+}
